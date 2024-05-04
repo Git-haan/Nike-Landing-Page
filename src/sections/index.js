073f2b932aa241f9f739659c156668b57a911d90
Git-haan <ishaan.reddy5@gmail.com>
2024-05-03 21:20:57 -0400

    implementation

--- src/sections/index.js
+++ src/sections/index.js
@@ -1,18 +1,18 @@
 import Hero from './Hero'
+import SpecialOffer from './SpecialOffer'
 import Popular from './Popular'
 import SuperQuality from './SuperQuality'
 import Services from './Services'
-import SpecialOffer from './SpecialOffer'
 import Reviews from './Reviews'
 import Subscription from './Subscription'
 import Footer from './Footer'
 
 export {
     Hero,
+    SpecialOffer,
     Popular,
     SuperQuality,
     Services,
-    SpecialOffer,
     Reviews,
     Subscription,
     Footer
